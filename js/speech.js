8e1517331161d8b516105e7286de42ac9d1c8adc
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-05 10:04:54 +0530

    Update speech.js

--- js/speech.js
+++ js/speech.js
@@ -3,7 +3,7 @@
 // =========================================
 
 // ===== CHANGE THIS WHEN YOU HAVE A MICROPHONE =====
-const DEMO_MODE = true;
+const DEMO_MODE = false;
 console.log("Demo mode:", DEMO_MODE);
 // false = Real Speech Recognition
 // true = Random Demo Results
