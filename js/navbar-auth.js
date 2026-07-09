ef15346ec7f7c6f6ceedaa9de81e267e5b6fe4b6
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-09 11:25:53 +0530

    Update navbar-auth.js

--- js/navbar-auth.js
+++ js/navbar-auth.js
@@ -1,32 +1,47 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 const authButtons = document.getElementById("authButtons");
 
 onAuthStateChanged(auth, (user) => {
 
     if (user) {
 
-        authButtons.innerHTML = `
-            <div id="profileButton" class="profile-button">
-                👤
-            </div>
-        `;
-        const profileButton = document.getElementById("profileButton");
-const profileOverlay = document.getElementById("profileOverlay");
-const closeProfile = document.getElementById("closeProfile");
+    authButtons.innerHTML = `
+        <div id="profileButton" class="profile-button">
+            👤
+        </div>
+    `;
 
-profileButton.addEventListener("click", () => {
-    profileOverlay.classList.add("show");
-});
+    const userDoc = await getDoc(doc(db, "users", user.uid));
 
-closeProfile.addEventListener("click", () => {
-    profileOverlay.classList.remove("show");
-});
+    if (userDoc.exists()) {
+
+        const data = userDoc.data();
+
+        document.getElementById("profileName").textContent = data.name;
+        document.getElementById("profileEmail").textContent = data.email;
 
     }
+
+    const profileButton = document.getElementById("profileButton");
+    const profileOverlay = document.getElementById("profileOverlay");
+    const closeProfile = document.getElementById("closeProfile");
+
+    profileButton.addEventListener("click", () => {
+        profileOverlay.classList.add("show");
+    });
+
+    closeProfile.addEventListener("click", () => {
+        profileOverlay.classList.remove("show");
+    });
+
+}
 
 });
