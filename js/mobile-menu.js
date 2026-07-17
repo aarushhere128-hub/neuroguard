509cf4e8043d12479a2ab972ba303ba8e987128b
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-17 12:34:40 +0530

    Create mobile-menu.js

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+
+    navMenu.classList.toggle("show");
+
+    hamburger.textContent =
+        navMenu.classList.contains("show")
+        ? "✕"
+        : "☰";
+
+});
